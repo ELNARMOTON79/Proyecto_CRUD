afd57f79d8f689089f3e83eb10da80d3cd50d35a
ELNARMOTON79 <rafaelalexandro6949@gmail.com>
2024-08-29 07:30:53 -0600

    Login arreglado

--- logins/login.php
+++ logins/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../styles/login.css">
+    <link rel="stylesheet" href="../styles/logins.css">
 </head>
 <div>
 <body>
