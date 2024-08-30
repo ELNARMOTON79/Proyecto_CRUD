317f4365cf59fd258a6336b811922c1217409f21
Rootkid314 <146594898+Rootkid314@users.noreply.github.com>
2024-08-29 22:12:01 -0600

    index correjido

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edu4All Index</title>
+    <title>Edu 4 All</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -14,19 +14,30 @@
 
 <body class="flex flex-col min-h-screen">
     <?php include 'navbar.php'; ?>
+
     <div class="flex-grow"></div>
-    
-    <div class="slider-box">
-        <ul>
-            <li>
-                <img src="SRC/Carrucel/imagen1.jpg" alt="">
-                <div class="texto">
-                    <h2>Imagen 1</h2>
-                </div>
-            </li>
-        </ul>
-    </div>
     <?php include 'footer.php'; ?>
+    <!--<footer class="bg-green-500 text-white py-8">
+        <div class="container mx-auto text-center">
+            <p class="fa-2x text-lg lg:mr-11">&copy; 2024.Todos los derechos reservados.</p>
+        </div>
+        <div class="container mx-auto text-center"></div>
+        <a href="index.php" class="text-white px-3 py-2 rounded-md hover:bg-green-700 transition duration-500 ease-in-out <?php echo is_active('index.php'); ?>">
+                    <i class="fa-solid fa-house"></i>    
+                        Home
+                    </a>
+                    <a href="about.php" class="text-white px-3 py-2 rounded-md hover:bg-green-700 transition duration-500 ease-in-out <?php echo is_active('about.php'); ?>">
+                    <i class="fa-solid fa-users-gear"></i>
+                    About Us
+                    </a>
+                    <a href="contact.php" class="text-white px-3 py-2 rounded-md hover:bg-green-700 transition duration-500 ease-in-out <?php echo is_active('contact.php'); ?>">
+                    <i class="fa-solid fa-phone"></i>
+                    Contact
+                    </a>
+                    <a href="profile.php" class="text-white px-3 py-2 rounded-md hover:bg-green-700 transition duration-500 ease-in-out <?php echo is_active('profile.php'); ?>">
+                        <i class="fa-solid fa-user"></i>
+                        Login
+    </footer>-->
 </body>
 
 </html>
