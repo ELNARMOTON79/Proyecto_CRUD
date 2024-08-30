0df5aa06f546fe28f0470723ea3309ba0d847e7c
ELNARMOTON79 <rafaelalexandro6949@gmail.com>
2024-08-30 15:08:06 -0600

    Redirecciones de redes sociales del footer agregadas

--- footer.php
+++ footer.php
@@ -3,13 +3,13 @@
         <div class="container mx-auto flex justify-between items-center mt-4">
             <hr class="h-1 w-1/3 bg-black border-0"> <!-- Línea izquierda -->
             <div class="flex space-x-6"> <!-- Espacio entre los íconos aumentado -->
-                <a href="profile.php" class="text-black hover:text-green-700 transition duration-500 ease-in-out text-3xl">
+                <a href="https://www.instagram.com/edu4all_oficial/" class="text-black hover:text-green-700 transition duration-500 ease-in-out text-3xl">
                     <i class="fa-brands fa-instagram"></i>
                 </a>
                 <a href="profile.php" class="text-black hover:text-green-700 transition duration-500 ease-in-out text-3xl">
                     <i class="fa-brands fa-facebook"></i>
                 </a>
-                <a href="profile.php" class="text-black hover:text-green-700 transition duration-500 ease-in-out text-3xl">
+                <a href="https://x.com/CaleidosOficial" class="text-black hover:text-green-700 transition duration-500 ease-in-out text-3xl">
                     <i class="fa-brands fa-twitter"></i>
                 </a>
                 <a href="profile.php" class="text-black hover:text-green-700 transition duration-500 ease-in-out text-3xl">
