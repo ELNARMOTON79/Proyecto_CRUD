6264ec9ff1a0b6fe859d71f42a2019d939d332cb
Jesus Rivera <146463870+LordAguaKate@users.noreply.github.com>
2024-09-23 11:12:05 -0600

    CRUD actividades creado

--- Dashboard_Admin/crear_actividades.php
+++ Dashboard_Admin/crear_actividades.php
@@ -130,7 +130,6 @@ $tipo_usuario = isset($_SESSION['tipo_usuario']) ? $_SESSION['tipo_usuario'] : '
     </div>
     <!-- Script para jalar el javascripts.js-->
     <script src="javascripts.js"></script>
-    <div></div>
 </body>
 
 </html>
