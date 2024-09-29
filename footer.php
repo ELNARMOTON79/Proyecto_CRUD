4cb9ed1550531be4976bdfeddc3c0ca1df25c5be
heidyguzman <145802661+heidyguzman@users.noreply.github.com>
2024-09-28 19:58:41 -0600

    foothericonos

--- footer.php
+++ footer.php
@@ -1,22 +1,24 @@
 <footer class="bg-green-500 text-white py-4">
     <div class="container mx-auto text-center">
-        <div class="container mx-auto flex justify-between items-center mt-4">
+        <div class="flex justify-between items-center mt-4">
             <hr class="h-1 w-1/3 bg-black border-0"> 
             <div class="flex space-x-6"> 
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
                 <a href="https://www.instagram.com/edu4all_oficial/" class="text-black hover:text-green-700 transition duration-500 ease-in-out text-3xl">
-                    <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-instagram"></i>
                 </a>
                 <a href="https://x.com/CaleidosOficial" class="text-black hover:text-green-700 transition duration-500 ease-in-out text-3xl">
-                    <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-twitter"></i>
                 </a>
             </div>
             <hr class="h-1 w-1/3 bg-black border-0"> 
         </div>
         <div>
-        <span class="text-x2 fa-2x font-bold ml-3">Edu4All</span>
+            <span class="text-2xl font-bold ml-3">Edu4All</span>
         </div> 
         <div>
-            <p class="fa-2x text-lg">Designed, built and maintained by <span class="text-white">team 5</span> &copy; 2024</p>
+            <p class="text-lg">Designed, built and maintained by <span class="text-white">team 5</span> &copy; 2024</p>
         </div>
     </div>
 </footer>
