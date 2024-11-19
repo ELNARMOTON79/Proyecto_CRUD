59e099bf53efe04232ab9a1878c15d46e07feb88
Rafael Alexandro Vuelvas Pérez <138848815+ELNARMOTON79@users.noreply.github.com>
2024-11-19 17:20:52 -0600

    News arreglado (no se ni que arregle

--- news.php
+++ news.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>News</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="icon" href="SRC/EDU4ALL..png">
 </head>
 <body class="bg-gray-200">
     <?php include ("navbar.php"); ?>
